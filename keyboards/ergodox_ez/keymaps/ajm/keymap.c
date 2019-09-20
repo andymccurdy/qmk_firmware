e9a631800f96aaefe9ac7ec9d8786ed030f94a5c
Andy McCurdy <andy@andymccurdy.com>
2019-09-20 11:16:51 -0700

    capslock key from MO(SYMBOLS) -> TAB

--- keyboards/ergodox_ez/keymaps/ajm/keymap.c
+++ keyboards/ergodox_ez/keymaps/ajm/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KC_TRANSPARENT,         KC_1,                   KC_2,                   KC_3,                   KC_4,                   KC_5,                   KC_ESCAPE,
     KC_TAB,                 KC_Q,                   KC_W,                   KC_E,                   KC_R,                   KC_T,                   KC_TRANSPARENT,
-    MO(SYMBOLS),            KC_A,                   KC_S,                   KC_D,                   KC_F,                   KC_G,
+    KC_TAB,                 KC_A,                   KC_S,                   KC_D,                   KC_F,                   KC_G,
     KC_LSHIFT,              KC_Z,                   KC_X,                   KC_C,                   KC_V,                   KC_B,                   KC_TRANSPARENT,
     KC_LCTRL,               KC_LGUI,                KC_MEDIA_PLAY_PAUSE,    KC_AUDIO_MUTE,          MO(MAC_NAV),
 
